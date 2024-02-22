ba6c45d249944d57f07db86ae9bcf1a74a467607
Jemimah-Nthenya <jemimahogonda@gmail.com>
2024-02-22 07:51:34 +0300

    add files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+fun main(){
+ printName("Jemimah")
+modulus(56,17)
+sum(57,83,59,65)
+    fact("travelling")
+}
+fun printName(name : String){
+    var name="hello $name"
+    println(name)
+}
+fun modulus(num1:Int,num2:Int){
+    var result=num1%num2
+    println(result)
+}
+fun sum(num1: Int,num2: Int,num3: Int,num4: Int) {
+    var result = num1 + num2 + num3 + num4
+    println(result)
+}
+fun fact(text: String){
+    var result=text
+    println(result)
+}
+
